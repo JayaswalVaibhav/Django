057886836214bf83fe31531fb94e770c290c18dc
JayaswalVaibhav <vaibhavjayaswal4180@gmail.com>
2021-07-31 17:21:23 +0530

    First Commit of textutils. It includes remove punctuation, new line remove, uppercase and character count

---
+++ textutil/textutil/views.py
@@ -0,0 +1,68 @@
+# i have created this file
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+    # return HttpResponse("Hello")
+
+
+def analyze(request):
+    # get the text
+    djtext = request.POST.get('text', 'default')
+    print(djtext)
+    # check checkbox value
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    charcount = request.POST.get('charcount', 'off')
+
+    # analyze the text
+    # if user wants to remove punctuation
+    if removepunc == 'on':
+        punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punc:
+                analyzed = ''.join((analyzed, char))
+        params = {'purpose': 'Remove Punctuation', 'analyze_text': analyzed, 'charcount': charcount}
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
+
+    # if user wants the text in uppercase
+    if fullcaps == "on":
+        analyzed = djtext.upper()
+        params = {'purpose': 'Changed to uppercase', 'analyze_text': analyzed, 'charcount': charcount}
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
+
+    # \n means new line. It means that the cursor must go to the next line. \r means carriage return. It means that
+    # the cursor should go back to the beginning of the line.
+    if newlineremover == 'on':
+        analyzed = ""
+        for char in djtext:
+            if char != '\n' and char != '\r':
+                analyzed = ''.join((analyzed, char))
+        params = {'purpose': 'New Line remove', 'analyze_text': analyzed, 'charcount': charcount}
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
+
+    # if user wants character count
+    if charcount == 'on':
+        analyzed = djtext
+        params = {'purpose': 'Character count',
+                  'analyze_text': analyzed,
+                  'charcount': charcount,
+                  'char_count': len(analyzed)}
+        # return render(request, 'analyze.html', params)
+
+    # if nothing has selected
+    if removepunc != "on" and fullcaps != "on" and newlineremover != "on" and charcount != "on":
+        return HttpResponse("Nothing has selected. Error")
+
+    return render(request, 'analyze.html', params)
+
+
+def about(request):
+    return render(request, 'about.html')
